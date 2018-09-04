d1af20ce2c11be6bbfaa698e25b793296f65aded
Unknown <rn2702@gmail.com>
2018-09-04 17:43:45 -0400

    Cambio puerto

--- HenriPittier/src/modelo/ComunicacionREST.java
+++ HenriPittier/src/modelo/ComunicacionREST.java
@@ -43,7 +43,7 @@ public class ComunicacionREST {
 
     private BufferedReader comunicar(String _etiquetaHTTP, String _urlMetodo) throws IOException {
         try {
-            URL url = new URL("http://"+ip+":8080/HenriPittierWeb/webresources/WS/" + _urlMetodo);
+            URL url = new URL("http://"+ip+":8085/HenriPittierWeb/webresources/WS/" + _urlMetodo);
             conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod(_etiquetaHTTP);
             conn.setRequestProperty("Accept", "application/json");
